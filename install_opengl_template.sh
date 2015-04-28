45fd15a8fd0f534170a2ebba331b2136bf92a727
colin3dmax <colin3dmax@163.com>
2015-04-28 09:38:07 +0800

    add New Template

--- install_opengl_template.sh
+++ install_opengl_template.sh
@@ -1,1 +1,2 @@
-sudo cp -R OpenGL.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+#sudo cp -R OpenGL.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+sudo cp -R OpenGL3.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
