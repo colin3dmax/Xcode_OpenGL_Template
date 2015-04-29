5e41c0550c7905f58bbe0576e22f136158a0f1c6
colin3dmax <colin@qidea.cc>
2015-04-29 17:16:44 +0800

    update opengl template

--- install_opengl_template.sh
+++ install_opengl_template.sh
@@ -1,5 +1,7 @@
 # echo "install freeglut"
 # sudo port install freeglut
+# echo "install glfw"
+# sudo port install glfw
 # echo "install Mac OpenGL2.x"
 # sudo cp -R OpenGL.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
 # echo "install Mac XWindow OpenGL3.x"
