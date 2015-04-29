fae3a06e71848a77718098fbe1159a47d3d55bc0
colin3dmax <colin@qidea.cc>
2015-04-29 16:18:27 +0800

    添加Xcode OpenGL GLFW模板

--- install_opengl_template.sh
+++ install_opengl_template.sh
@@ -1,2 +1,9 @@
-#sudo cp -R OpenGL.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
-sudo cp -R OpenGL3.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+# echo "install freeglut"
+# sudo port install freeglut
+# echo "install Mac OpenGL2.x"
+# sudo cp -R OpenGL.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+# echo "install Mac XWindow OpenGL3.x"
+# sudo cp -R OpenGL3.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+echo "install Mac OpenGL GLFW"
+sudo cp -R OpenGL_GLFW.xctemplate /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/
+
